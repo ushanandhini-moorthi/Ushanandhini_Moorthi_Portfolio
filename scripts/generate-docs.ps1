fad8e6759f28d1ea74e1fd6c1dda8c01ef230ed8
ushanandhini-moorthi <ushanandhini.moorthi@gmail.com>
2026-02-04 13:57:25 +0530

    outputfiles

---
+++ scripts/generate-docs.ps1
@@ -0,0 +1,9 @@
+cd "C:\Users\Sudhakar\Documents\Ushanandhini Moorthi\Portfolio\Ushanandhini_Moorthi_Portfolio"
+
+# Generate HTML
+pandoc "facebook-qsg\source\markdown\quick_Start.md" -o "facebook-qsg\output\html\quick_Start.html" --from=gfm+hard_line_breaks --standalone --resource-path="facebook-qsg/source/markdown;facebook-qsg/assets"
+
+# Generate PDF
+pandoc "facebook-qsg\source\markdown\quick_Start.md" -o "facebook-qsg\output\pdf\quick_Start.pdf" --from=gfm+hard_line_breaks --pdf-engine=xelatex --resource-path="facebook-qsg/source/markdown;facebook-qsg/assets" -V geometry:margin=1in -V mainfont="Times New Roman"
+
+Write-Host "Done! HTML and PDF generated."
